fc944c92367cbb8d0cc6d37639368ecf260b8e19
vikash-kanauja <vikashkanoujia@gmail.com>
2024-06-20 21:12:31 +0530

    first commit

---
+++ src/components/Square.js
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+
+function Square({value,onSquareClick}) {
+  return (
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
+  );
+}
+
+export default Square;
